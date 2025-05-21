bb4c6d8e518eb6471ea68d5902e098df1a8055f6
Belmiro M. Kunga <belmirokunga@gmail.com>
2025-05-21 14:40:24 +0100

    "Fix MIME
    "Fix MIME type errors and improve asset handling"

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,8 @@ export default defineConfig({
   build: {
     target: 'esnext',
     minify: true,
+    outDir: 'dist',
+    assetsDir: 'assets',
     rollupOptions: {
       input: {
         main: './index.html'
